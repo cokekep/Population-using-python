669612e7edb1573f762e932efc926055a542a1b0
Chinonso Okeke <cokekep8@gmail.com>
2020-06-10 19:27:41 +0200

    code refactor

--- pand.py
+++ pand.py
@@ -1,18 +1,10 @@
 import pandas as pd
-result = {}
 df = pd.read_csv('nga_pop_sendist_2016.csv')
 pop = dict(df.groupby(['admin1Name_en'])['Population2016'].sum())
-for key, value in pop.items():
-    if value >= 5000000:
-        result[key] = value
-print(result)
-    #break
-#for key, value in pop.items():
- #   if value > 5000000:
-  #      result.append(pop)
-   #     print (result)
-    #    break
-    #else:
-     #   break
-#print(df[["SenDist_en", "Population2016"]].head())
-#print(df.head())
+def popSum(data):
+    result = {}
+    for key, value in pop.items():
+        if value >= 5000000:
+            result[key] = value
+    return result
+print(popSum(pop))
