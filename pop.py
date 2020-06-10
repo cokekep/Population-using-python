6d63754367b77c09fc2a94381948aa20e83e4258
Chinonso Okeke <cokekep8@gmail.com>
2020-06-10 15:43:49 +0200

    changes edited

--- pop.py
+++
@@ -1,6 +0,0 @@
-import csv
-with open('C:\\Users\pc\\Desktop\Python\\nga_pop_sendist_2016.csv', mode ='r') as myfile:
-    readFile = csv.DictReader(myfile)
-    for lines in readFile:
-        if 
-        print(lines[1])
