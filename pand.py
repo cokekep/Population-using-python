4c8839dd6b2c737bf54637c91b910d94da4d3df9
Chinonso Okeke <cokekep8@gmail.com>
2020-06-10 20:15:50 +0200

    second refactor

--- pand.py
+++ pand.py
@@ -1,10 +1,11 @@
 import pandas as pd
-df = pd.read_csv('nga_pop_sendist_2016.csv')
-pop = dict(df.groupby(['admin1Name_en'])['Population2016'].sum())
+pop = {}
 def popSum(data):
+    df = pd.read_csv('nga_pop_sendist_2016.csv')
+    pop = dict(df.groupby(['admin1Name_en'])['Population2016'].sum())
     result = {}
     for key, value in pop.items():
         if value >= 5000000:
             result[key] = value
-    return result
+    return result   
 print(popSum(pop))
